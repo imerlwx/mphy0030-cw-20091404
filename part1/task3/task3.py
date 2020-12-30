6bfa51941c5de292aeb6682ec3a4d7ffef94a35e
imerlwx <72551690+imerlwx@users.noreply.github.com>
2020-12-30 22:25:10 +0800

    Edit task3
    Change to a totally different file, the file before is totally wrong. But there is still some problems, why the finite difference after each iteration doesn't change?

--- part1/task3/task3.py
+++ part1/task3/task3.py
@@ -1,51 +1,67 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
-## define a function to automatically compute gradient
-def finite_difference_gradient(x, y, a, h, learning_rate):
-    
-    m = float(len(x)) # number of data sets
-    k = float(len(a)) # number of parameters
+## define a function to compute gradient by using finite fifference
+## finite difference uses the value from packward and forward point
+def finite_difference_gradient(f_plus, f_minus, dx):
 
-    # for the cost function 'J' we know that its gradient about   
-    # 'a[k]' is just (h[i] - y[i]) * x[k] / m
-    # get the 'a' after gradient
-    for k in range(0, k):
-        for i in range(0, m):
-            a[k] -= learning_rate * (h[i] - y[i]) * x[k] / m
+    h = (f_plus - f_minus) / 2 * dx
 
-    return a, m
-    
+    return h
 
-## define a function to compute cost fuction and find its minimum
-def gradient_descent(a_intial, x, y):
+## define a function to compute the minimum of a given polynomial
+def gradient_descent(a, x):
     
     # define basic parameter for gradient
-    learning_rate = 0.0001
-    iteration_num = 300
-    tolerance = 0.001
-
-    a = a_intial 
-    h = x * a  # get intial a and h
-    totalError = np.zeros((iteration_num, 1))  # set up error of gradient descent result
+    step_size = 0.5
+    iteration_num = 100
+    tolerance = 0.00001
+    dx = 0.1
 
     n = 1 # intial iteration number
+    f = np.zeros((iteration_num, 1)) # set up intial f
+    f[0] = a @ x
 
     # compute the cost function J(a) and iterate it
-    while n <= iteration_num:
+    while n < iteration_num:
+        
+        f_plus = a @ (x + dx) 
+        f_minus = a @ (x - dx) 
 
-        a, m = finite_difference_gradient(x, y, a, h, learning_rate)
+        h = finite_difference_gradient(f_plus, f_minus, dx) # get gradient using finite_difference_gradient
+        
+        for k in range(len(x)):
+            x[k, 0] += -step_size * h  # gradient descent
 
-        h = x * a # update the multivariate function
+        f[n] = a @ x
 
-        for i in range(0, m):
-            totalError[n] += ((h[i] - y[i]) ** 2) / (2 * m) # compute the cost function result
-    
-        print(f"Iteration: {n}, totalError: {totalError[n]}")
+        #print(f"Iteration: {n}, f(x): {f[n]}")
+        print("Iteration: %d, f(x): %f" %(n, f[n]))
 
-        if totalError[n] - totalError[n-1] > tolerance:
-            n += 1
-        else:
+        # if the change of f is smaller than tolerance, then stop the iteration
+        if f[n-1] - f[n] < tolerance:
             break
+        else:
+            n += 1
 
+    #fig = plt.figure()
+    m = np.arange(0, iteration_num).reshape(iteration_num, 1)
+    plt.plot(m, f)
+    plt.show()
+
+    return f
+
+## implement gradient descent to a quadratic polynomial 
 def quadratic_polynomial():
     
+    # set up intial a, x, f
+    a = np.random.rand(1, 10)
+    x_intial = np.array([[0.1], [0.1], [0.1]])
+
+    x = np.array([x_intial[0] ** 2, x_intial[1] ** 2, x_intial[2] ** 3, 
+                x_intial[0] * x_intial[1], x_intial[0] * x_intial[2], 
+                x_intial[1] * x_intial[2], x_intial[0], x_intial[1], x_intial[2], [1.]]) 
+
+    gradient_descent(a, x)
+
+quadratic_polynomial()
