8e61ea34b383730e95685414ebd5dfdeadad08da
imerlwx <72551690+imerlwx@users.noreply.github.com>
2020-12-31 12:03:55 +0800

    Edit task3
    Finish task3, solved all the problems before.

--- part1/task3/task3.py
+++ part1/task3/task3.py
@@ -9,59 +9,63 @@ def finite_difference_gradient(f_plus, f_minus, dx):
 
     return h
 
+## define a polynomial
+def quadratic_polynomial(a, x):
+
+    poly = np.array([x[0] ** 2, x[1] ** 2, x[2] ** 3, x[0] * x[1],
+                 x[0] * x[2], x[1] * x[2], x[0], x[1], x[2], [1.]])  
+
+    f = a @ poly
+    
+    return f
+
 ## define a function to compute the minimum of a given polynomial
 def gradient_descent(a, x):
     
     # define basic parameter for gradient
-    step_size = 0.5
-    iteration_num = 100
-    tolerance = 0.00001
+    step_size = 0.1
+    iteration_num = 1000
+    tolerance = 0.0001
     dx = 0.1
 
     n = 1 # intial iteration number
     f = np.zeros((iteration_num, 1)) # set up intial f
-    f[0] = a @ x
+    f[0] = quadratic_polynomial(a, x)
 
     # compute the cost function J(a) and iterate it
     while n < iteration_num:
         
-        f_plus = a @ (x + dx) 
-        f_minus = a @ (x - dx) 
+        f_plus = quadratic_polynomial(a, x + dx)
+        f_minus = quadratic_polynomial(a, x - dx)
 
         h = finite_difference_gradient(f_plus, f_minus, dx) # get gradient using finite_difference_gradient
         
         for k in range(len(x)):
             x[k, 0] += -step_size * h  # gradient descent
+        
+        f[n] = quadratic_polynomial(a, x)
 
-        f[n] = a @ x
-
-        #print(f"Iteration: {n}, f(x): {f[n]}")
         print("Iteration: %d, f(x): %f" %(n, f[n]))
 
         # if the change of f is smaller than tolerance, then stop the iteration
-        if f[n-1] - f[n] < tolerance:
+        if abs(f[n-1] - f[n]) < tolerance:
+            print("It uses %d iterations to reach the minimum %f" %(n, f[n]))
             break
         else:
             n += 1
 
-    #fig = plt.figure()
-    m = np.arange(0, iteration_num).reshape(iteration_num, 1)
-    plt.plot(m, f)
-    plt.show()
-
-    return f
-
-## implement gradient descent to a quadratic polynomial 
-def quadratic_polynomial():
+    return n, f
     
-    # set up intial a, x, f
-    a = np.random.rand(1, 10)
-    x_intial = np.array([[0.1], [0.1], [0.1]])
+# randomly generate a and input a estimate of x
+a = np.random.rand(1, 10)
+x = np.array([[1.], [1.], [1.]])
 
-    x = np.array([x_intial[0] ** 2, x_intial[1] ** 2, x_intial[2] ** 3, 
-                x_intial[0] * x_intial[1], x_intial[0] * x_intial[2], 
-                x_intial[1] * x_intial[2], x_intial[0], x_intial[1], x_intial[2], [1.]]) 
+n, f = gradient_descent(a, x)
 
-    gradient_descent(a, x)
-
-quadratic_polynomial()
+# plot the change of f after n iterations to make sure it's decreasing
+m = np.arange(0, n).reshape(n, 1)
+plt.title("Change of f after n iterations")
+plt.xlabel("iterations")
+plt.ylabel("quadratic polynomial value")
+plt.plot(m, f[0:n, 0])
+plt.show()
