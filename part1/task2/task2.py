331a90549b9a1634517fd8bffd228149900bac0a
imerlwx <72551690+imerlwx@users.noreply.github.com>
2021-01-02 11:18:05 +0800

    Edit task2
    It can plot a normal bivariate Gaussian probability density surface now

--- part1/task2/task2.py
+++ part1/task2/task2.py
@@ -1,37 +1,28 @@
 import math
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 
 ## define a function to output the probability densities 
 def bi_gaussian_pdf(x, miu, sigma):
     
-    p = (np.exp(-((x - miu).transpose()) @ (np.linalg.inv(sigma)) @ 
-        (x - miu) / 2)) / (2 * math.pi * math.sqrt(np.linalg.det(sigma)))
+    p_intial = (np.exp(((x - miu).transpose()) @ (np.linalg.inv(sigma)) @ 
+        (x - miu) / (-2))) / (2 * math.pi * math.sqrt(np.linalg.det(sigma)))
+
+    p = np.diagonal(p_intial)
 
     return p
 
-x = np.random.rand(2, 1000) # set a random x
+x = np.random.rand(2, 10000) # set a random x
 miu = np.mean(x, axis=1).reshape(2, 1) # compute the average of x in each row
-n = x.shape[1] # get the number of column in x
-
-# compute the covariance matrix
-sigma = np.zeros((2,2))
-sigma_x1 = np.sum((x[0] - miu[0]) ** 2) / (n - 1)
-sigma_x2 = np.sum((x[1] - miu[1]) ** 2) / (n - 1)
-sigma_x1x2 = np.sum((x[0] - miu[0]) * (x[1] - miu[1])) / (n - 1)
-sigma[0, 0] = sigma_x1
-sigma[0, 1] = sigma_x1x2
-sigma[1, 0] = sigma_x1x2
-sigma[1, 1] = sigma_x2
+sigma = np.cov(x) # compute the covariance matrix
 
 p = bi_gaussian_pdf(x, miu, sigma)
 
+fig = plt.figure()
+ax = Axes3D(fig)
+surf = ax.plot_trisurf(x[0], x[1], p, cmap = cm.coolwarm, alpha = 0.5)
+fig.colorbar(surf, shrink=0.5, aspect=5)
 
-plt.plot(x[0], p[0, :])
-
-#fig = plt.figure()
-#ax = fig.gca(projection='3d')
-#X, Y = np.meshgrid(x[0], x[1])
-#ax.plot_surface(X, Y, p)
-#ax.legend()
 plt.show()
