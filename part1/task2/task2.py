32355504a19a3d3a8a53d90cd00410c32fd73341
imerlwx <72551690+imerlwx@users.noreply.github.com>
2021-01-02 22:00:24 +0800

    Edit task2
    plot three ellipsoid surfaces, almost finish

--- part1/task2/task2.py
+++ part1/task2/task2.py
@@ -3,6 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.interpolate import griddata
 
 ## define a function to output the probability densities 
 def bi_gaussian_pdf(x, miu, sigma):
@@ -20,9 +21,23 @@ sigma = np.cov(x) # compute the covariance matrix
 
 p = bi_gaussian_pdf(x, miu, sigma)
 
+## draw a 3D triangular surface of p
 fig = plt.figure()
-ax = Axes3D(fig)
+
+ax = Axes3D(fig) 
 surf = ax.plot_trisurf(x[0], x[1], p, cmap = cm.coolwarm, alpha = 0.5)
 fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.title("Bivariate Gaussian Probability Density Surface")
+
+## draw the three ellipsoid surface 
+fig = plt.figure()
+
+# redefine x, y, z grids by using interpolation
+grid_x,grid_y = np.mgrid[np.min(x[0]):np.max(x[0]):0.001, np.min(x[1]):np.max(x[1]):0.001]
+grid_z = griddata(x.transpose(), p.transpose(), (grid_x, grid_y), method='nearest')
+a = np.max(p) # compute the maxmium of p to compute pencentiles
+cs = plt.contour(grid_x, grid_y ,grid_z, [0.1 * a, 0.5 * a, 0.9 * a], alpha = 0.75, cmap = cm.jet)
+plt.colorbar()
+plt.title("10th, 50th, 90th percentiles of the probability densities")
 
 plt.show()
