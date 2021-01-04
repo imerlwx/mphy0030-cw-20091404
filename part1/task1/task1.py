dfa63dd603f05fbf9d681a93cf4f1c21b37bb01a
imerlwx <72551690+imerlwx@users.noreply.github.com>
2021-01-04 10:52:08 +0800

    Edit task1
    Finish the two functions

--- part1/task1/task1.py
+++ part1/task1/task1.py
@@ -8,11 +8,11 @@ def simple_image_write(intensity, vox_dimension):
     # open a new file to write in binary format
     with open('.//data//image.sim', 'wb+') as fw:
         
-        a = np.shape(intensity)  # get the size of image
+        # store the image size to the header of file
+        image_size = struct.pack('i', np.size(intensity))
+        fw.write(image_size)
         
-        for i in range(len(a)):
-            image_size = struct.pack('h', a)
-            fw.write(image_size)
+        a = np.shape(intensity)  # get the shape of image
         
         for i in range(a[0]):
             for j in range(a[1]):
@@ -27,13 +27,25 @@ def simple_image_write(intensity, vox_dimension):
                 s2 = struct.pack('f', vox_dimension[i, j])
                 fw.write(s2)
 
+    return a, b
 
 ## read any binary file
-def simple_image_read(file_name):
+def simple_image_read(file_name, a, b):
 
     with open('.//data//image.sim', 'rb+') as fr:
         
-        image_size = struct.unpack
+        
+        image_size, = struct.unpack('i', fr.read(4))
+
+        # read intensity as an array
+        intensity_read_tuple = struct.unpack('{}h'.format(image_size), fr.read(2 * image_size))
+        intensity_read = np.array(intensity_read_tuple)
+        intensity = intensity_read.reshape(a)
+
+        # read voxel dimension as an array
+        vox_dimension_read_tuple = struct.unpack('{}f'.format(b[1]), fr.read(4 * b[0] * b[1]))
+        vox_dimension_read = np.array(vox_dimension_read_tuple)
+        vox_dimension = vox_dimension_read.reshape(b)
 
 
 
