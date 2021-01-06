ad66bbd948afe9c964f9e2acf8ef4271ed7bb4b2
imerlwx <72551690+imerlwx@users.noreply.github.com>
2021-01-06 19:14:55 +0800

    Edit task4
    Almost finished task4

--- part1/task4/task4.py
+++ part1/task4/task4.py
@@ -1,55 +1,63 @@
 import numpy as np
-from scipy.spatial import cKDTree as kdtree
+from scipy.spatial import KDTree as kdtree
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 ## define a function to implement lowpass smoothing algorithm
 def low_pass_smoothing(vertices, iterations, lambda1, mu):
-
-    n = 0
     
     vertices_smoothed = np.zeros(np.shape(vertices))
     
-    while n < iterations:
+    for n in range(iterations):
         for i in range(vertices.shape[0]):
             
-            _, idx = kdtree.query(vertices[i], vertices, k = 1)
+            vertices_tree = kdtree(vertices)
+            _, idx = vertices_tree.query(vertices[i], k = 7)
             
             q = np.zeros((1, 3)) # set the intial sum of nearest points
-            m = len(idx) # number of nearest points
+            m = len(idx) # number of nearest points (actually minus 1 because this including the point itself)
         
             for j in range(m):
                 q += vertices[idx[j]]
             
             if n % 2 == 0:
-                vertices_smoothed[i] = vertices[i] + lambda1 * (q / m - vertices[i])
+                vertices_smoothed[i] = vertices[i] + (q - m * vertices[i]) * lambda1 / (m - 1)
             else:
-                vertices_smoothed[i] = vertices[i] + mu * (q / m - vertices[i])
+                vertices_smoothed[i] = vertices[i] + (q - m * vertices[i]) * mu / (m - 1)
 
         vertices = vertices_smoothed
 
     return vertices
 
-vertices = np.genfromtxt('../data/example_vertices.csv',delimiter=',')
-triangles = np.genfromtxt('../data/example_triangles.csv',delimiter=',')
-
-# plot the first figure before smoothing
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-plt.subplot(1, 2, 1)
-ax.plot_trisurf(vertices[:,0], vertices[:,1], vertices[:,2], triangles = triangles, color='b')
+vertices_intial = np.genfromtxt('./data/example_vertices.csv',delimiter=',')
+triangles = np.genfromtxt('./data/example_triangles.csv',delimiter=',') - 1
 
 # set up parameters
-iterations = 10
 lambda1 = 0.9
 mu = -1.02
 
-vertices = low_pass_smoothing(vertices, iterations, lambda1, mu)
-
-# plot the second figure after smoothing
+# plot the first figure before smoothing
 fig = plt.figure()
-ax = fig.gca(projection='3d')
-plt.subplot(1, 2, 2)
+ax = fig.add_subplot(221, projection='3d')
+ax.plot_trisurf(vertices_intial[:,0], vertices_intial[:,1], vertices_intial[:,2], triangles = triangles, color='b')
+plt.title('before filtering')
+
+# plot the second figure after 5 iterations' smoothing
+ax = fig.add_subplot(222, projection='3d')
+vertices = low_pass_smoothing(vertices_intial, 5, lambda1, mu)
 ax.plot_trisurf(vertices[:,0], vertices[:,1], vertices[:,2], triangles = triangles, color='r')
+plt.title('after 5 iterations filtering')
+
+# plot the second figure after 10 iterations' smoothing 
+ax = fig.add_subplot(223, projection='3d')
+vertices = low_pass_smoothing(vertices_intial, 10, lambda1, mu)
+ax.plot_trisurf(vertices[:,0], vertices[:,1], vertices[:,2], triangles = triangles, color='r')
+plt.title('after 10 iterations filtering')
+
+# plot the second figure after 25 iterations' smoothing 
+ax = fig.add_subplot(224, projection='3d')
+vertices = low_pass_smoothing(vertices_intial, 25, lambda1, mu)
+ax.plot_trisurf(vertices[:,0], vertices[:,1], vertices[:,2], triangles = triangles, color='r')
+plt.title('after 25 iterations filtering')
 
 plt.show()
