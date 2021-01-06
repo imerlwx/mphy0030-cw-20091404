d12749c8a5175e0b6d0792aeb4f4bae931ec0bfb
imerlwx <72551690+imerlwx@users.noreply.github.com>
2021-01-06 21:52:22 +0800

    Edit task1
    Finished task1

--- part1/task1/task1.py
+++ part1/task1/task1.py
@@ -1,12 +1,15 @@
 import numpy as np
 import scipy.io as scio
 import struct
+from matplotlib import pyplot as plt
 
 ## write any file into a binary file
-def simple_image_write(intensity, vox_dimension):
+def simple_image_write(data):
+    
+    [intensity, vox_dimension] = [data['vol'], data['voxdims']] # read information from data
     
     # open a new file to write in binary format
-    with open('.//data//image.sim', 'wb+') as fw:
+    with open('./data/image.sim', 'wb+') as fw:
         
         # store the image size to the header of file
         image_size = struct.pack('i', np.size(intensity))
@@ -14,6 +17,7 @@ def simple_image_write(intensity, vox_dimension):
         
         a = np.shape(intensity)  # get the shape of image
         
+        # store the intensity value one by one to the file
         for i in range(a[0]):
             for j in range(a[1]):
                 for k in range(a[2]):
@@ -22,6 +26,7 @@ def simple_image_write(intensity, vox_dimension):
     
         b = np.shape(vox_dimension) # get the dimension of voxel dimensions
 
+        # store the voxel dimension one by one to the file
         for i in range(b[0]):
             for j in range(b[1]):
                 s2 = struct.pack('f', vox_dimension[i, j])
@@ -32,26 +37,39 @@ def simple_image_write(intensity, vox_dimension):
 ## read any binary file
 def simple_image_read(file_name, a, b):
 
-    with open('.//data//image.sim', 'rb+') as fr:
+    with open(file_name, 'rb+') as fr:
         
-        
+        # read the image size first
         image_size, = struct.unpack('i', fr.read(4))
 
-        # read intensity as an array
+        # read intensity value as an array
         intensity_read_tuple = struct.unpack('{}h'.format(image_size), fr.read(2 * image_size))
         intensity_read = np.array(intensity_read_tuple)
         intensity = intensity_read.reshape(a)
 
         # read voxel dimension as an array
-        vox_dimension_read_tuple = struct.unpack('{}f'.format(b[1]), fr.read(4 * b[0] * b[1]))
+        vox_dimension_read_tuple = struct.unpack('{}f'.format(b[0] * b[1]), fr.read(4 * b[0] * b[1]))
         vox_dimension_read = np.array(vox_dimension_read_tuple)
         vox_dimension = vox_dimension_read.reshape(b)
 
+    return intensity, vox_dimension
 
-
-
-
-dataFile = './/data//example_image.mat'
+# get data from a mat file
+dataFile = './data/example_image.mat'
 data = scio.loadmat(dataFile)
-[intensity, vox_dimension] = [data['vol'], data['voxdims']]
-    
+
+# write and read the binary file
+a, b = simple_image_write(data)
+intensity, vox_dimension = simple_image_read('./data/image.sim', a, b)    
+
+fig = plt.figure()
+ax1 = fig.add_subplot(131)
+ax1.imshow(intensity[:, :, 5], cmap = plt.cm.gray)
+
+ax2 = fig.add_subplot(132)
+ax2.imshow(intensity[:, :, 15], cmap = plt.cm.gray)
+
+ax2 = fig.add_subplot(133)
+ax2.imshow(intensity[:, :, 25], cmap = plt.cm.gray)
+
+plt.show()
